34e79fe7d1ddc36346440300f5149dc8f44dc40e
Jackson North <93577613+NyoomNyoom@users.noreply.github.com>
2023-12-16 10:33:12 +0000

    started work on advent of code day 3

---
+++ Day 3/main.py
@@ -0,0 +1,14 @@
+def readFile(fileName):
+    file = open(fileName)
+
+    return file
+
+def getLines(fileIn):
+    return fileIn.readlines()
+
+def matrixConversion(allLines):
+    numlines = 0
+    for line in allLines:
+        count += 1
+    
+    
