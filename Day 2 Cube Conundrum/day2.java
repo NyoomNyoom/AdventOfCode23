7be4ad2ce1f4ef0c656ff5817b7e645f2c6976dc
NyoomNyoom <jacksonnorth1275@gmail.com>
2023-12-17 16:00:18 +1300

    Starting work on day 2 :)

---
+++ Day 2 Cube Conundrum/day2.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class day2 {
+
+    public static int[] informationGather(String line){
+        int gameCounter = 0;
+        
+
+
+        for(int i = 0; i < line.length(); i++){
+            if(line.charAt(i) == 'b'){
+
+            }
+        }
+
+        return [0,1];
+    }
+
+    public static void main(String[] args) {
+        try{
+            Scanner sc = new Scanner(new File("A:/Coding/AdventOfCode23/Day 2 Cube Conundrum/input.txt"));
+
+            while(sc.hasNextLine()){
+
+            }
+        } catch (FileNotFoundException e){
+
+        }    
+    }
+}
